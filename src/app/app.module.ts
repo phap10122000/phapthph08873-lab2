ec2e88f505eb90efca1d6d815cf6c6b19eb83c9c
phap10122000 <trinhhaphap@gmail.com>
2020-03-23 12:54:42 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SliderComponent } from './slider/slider.component';
@@ -9,21 +9,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { ProdcutService } from './prodcut.service';
-import { ProductService } from './product.service';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { ServiceComponent } from './service/service.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { AboutComponent } from './about/about.component';
+
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, SliderComponent,  ProductListComponent, ProductComponent, CategoryComponent,ProductManagerComponent, HomeComponent, ContactComponent, ServiceComponent, ProductAddComponent, ProductDetailComponent, ProductEditComponent, AboutComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, SliderComponent,  ProductListComponent, ProductComponent, CategoryComponent,ProductManagerComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [ProdcutService, ProductService]
+  providers: []
 })
 export class AppModule { }
